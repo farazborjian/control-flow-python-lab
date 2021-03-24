1edc25e582448e6d7df93f23e810665f4201e32e
Faraz Borjian <farazborjian@Farazs-MacBook-Pro.local>
2021-03-24 15:57:49 -0700

    end of exe-6.py

--- exe-6.py
+++ exe-6.py
@@ -13,29 +13,22 @@
 # 4. Print the result as follows:
 #      <Mmm> <dd> is in <season> 
 
-# Hints:
-# Consider using the in operator to check if a string is in a particular list/tuple like this:
-# if input_month in ('Jan', 'Feb', 'Mar'):
-#
-# After setting the likely season, you can use another if...elif...else statement to "adjust" if
-# the day number falls within a certain range.
-
-# mo = input('Enter the month of the season (Jan - Dec): ')
-# day = int(input('Enter the day of the month: '))
-# if mo in ('Jan', 'Feb', 'Mar'):
-#   season = 'Winter'
-# elif mo in ('Apr', 'May', 'Jun'):
-#   season = 'Spring'
-# elif mo in ('Jul', 'Aug', 'Sep'):
-#   season = 'Summer'
-# else:
-#   season = 'Fall'
-# if mo == 'Mar' and day > 19:
-#   season = 'Spring'
-# elif mo == 'Jun' and day > 20:
-#   season = 'Summer'
-# elif mo == 'Sep' and day > 21:
-#   season = 'Fall'
-# elif mo == 'Dec' and day > 20:
-#   season = 'Winter'
-# print(f'{mo} {day} is in {season}')
+mo = input('Enter the month of the season (Jan - Dec): ')
+day = int(input('Enter the day of the month: '))
+if mo in ('Jan', 'Feb', 'Mar'):
+ season = 'Winter'
+elif mo in ('Apr', 'May', 'Jun'):
+   season = 'Spring'
+elif mo in ('Jul', 'Aug', 'Sep'):
+  season = 'Summer'
+else:
+   season = 'Fall'
+if mo == 'Mar' and day > 19:
+   season = 'Spring'
+elif mo == 'Jun' and day > 20:
+   season = 'Summer'
+elif mo == 'Sep' and day > 21:
+  season = 'Fall'
+elif mo == 'Dec' and day > 20:
+  season = 'Winter'
+print(f'{mo} {day} is in {season}')
